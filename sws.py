7c1ee4c7466ba59617f07ad461da32aac40c9fea
plmurdoch <pmurdoch@uvic.ca>
2022-09-27 11:24:13 -0700

    Update sws.py

--- sws.py
+++ sws.py
@@ -22,24 +22,30 @@ def listen_for_sockets(server):
         readable, writable, exceptional = select.select(inputs, outputs, inputs)
         for s in readable:
             if s == server:
-            #1.if s is server, accept new connection and append new connection socket to the list
-            else: 
+                s.accept()
+                inputs.append(s)
+            else:
                 message = s.recv(1024).decode()
             if message:
-                request_message[s] = request_message[s] + message
-                #2.if find "\r\n\r\n" or "\n\n" at the end of message we can process whole
-                    whole_message = request_message[s]
-                    outputs.append(s)
-                    #3.for each line in whole_message:
-                        #4.if not in correct format: "GET /filename HTTP/1.0"
-                            response_messages{s} += "HTTP/1.0 400 Bad Request"
-                        else:
-                            #5. response messages with more oomf
-                            response_messages{s} += "HTTP/1.0 200 OK"
-                
-        
-    
-    
+                message_reader(message, response_messages, request_message)
+
+
+def message_reader(message, response, request):
+    limit_string_1 = "\r\n\r\n"
+    limit_string_2 ="\n\n"
+    while message != limit_string_1 or message != limit_string_2:
+        request_message[s] = request_message[s] + message
+    whole_message = request_message[s]
+    outputs.append(s)
+    for lines in whole_message:
+        if lines != 
+        #3.for each line in whole_message:
+            #4.if not in correct format: "GET /filename HTTP/1.0"
+                response_messages{s} += "HTTP/1.0 400 Bad Request"
+            else:
+                #5. response messages with more oomf
+                response_messages{s} += "HTTP/1.0 200 OK"
+
 
 
 def main():
@@ -49,6 +55,6 @@ def main():
     ip_addy = sys.argv[1]
     port_num = sys.argv[2]
     open_simple_web_server(ip_addy, port_num)
-    
+
 if __name__ == "__main__":
     main()
